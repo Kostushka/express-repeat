2caf0fee42617e7dffd04b8b7c8ecf75a91b72b3
Kostushka <nastyatery47@yandex.ru>
2022-10-12 15:54:44 +0300

    Added html, scss and file structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,65 @@
-import styles from './App.module.css';
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './containers/Content/Categories';
+import Sort from './containers/Content/Sort';
+import Title from './containers/Content/Title';
+import Item from './containers/Content/Item';
 
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <div className='wrapper'>
+                <Header />
+                <div className='content'>
+                    <div className='container'>
+                        <div className='content__top'>
+                            <Categories />
+                            <Sort />
+                        </div>
+                        <Title />
+                        <div className='content__items'>
+                            <Item
+                                img='https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg'
+                                title='Чизбургер-пицца'
+                            />
+                            <Item
+                                img='https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg'
+                                title='Чизбургер-пицца'
+                            />
+                            <Item
+                                img='https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg'
+                                title='Чизбургер-пицца'
+                            />
+                            <Item
+                                img='https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg'
+                                title='Чизбургер-пицца'
+                            />
+                            <Item
+                                img='https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg'
+                                title='Чизбургер-пицца'
+                            />
+                            <Item
+                                img='https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg'
+                                title='Чизбургер-пицца'
+                            />
+                            <Item
+                                img='https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg'
+                                title='Чизбургер-пицца'
+                            />
+                            <Item
+                                img='https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg'
+                                title='Чизбургер-пицца'
+                            />
+                            <Item
+                                img='https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg'
+                                title='Чизбургер-пицца'
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/containers/Content/Title/Title.jsx
@@ -0,0 +1,4 @@
+function Title() {
+    return <h2 className='content__title'>Все пиццы</h2>;
+}
+export default Title;
