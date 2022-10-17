5659592ef73f970f28468621f447100ebeb95b54
Kostushka <nastyatery47@yandex.ru>
2022-10-17 10:06:23 +0300

    Added list render and state for active className

--- src/containers/Content/Categories/Categories.jsx
+++ src/containers/Content/Categories/Categories.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
+
 function Categories() {
+    const [state, setState] = useState(0);
+
+    const list = [
+        'Все',
+        'Мясные',
+        'Вегетарианская',
+        'Гриль',
+        'Острые',
+        'Закрытые',
+    ];
+
     return (
         <div className='categories'>
             <ul>
-                <li className='active'>Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {list.map((el, i) => (
+                    <li
+                        onClick={() => setState(i)}
+                        className={state === i ? 'active' : ''}
+                        key={i}
+                    >
+                        {el}
+                    </li>
+                ))}
             </ul>
         </div>
     );
