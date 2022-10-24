bf8adca863f8c7c982656df2be9d4c910b4a0a43
Kostushka <nastyatery47@yandex.ru>
2022-10-24 20:43:17 +0300

    Added fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './scss/app.scss';
-import pizzaList from './assets/pizzas.json';
 import Header from './components/Header';
 import Categories from './containers/Content/Categories';
 import Sort from './containers/Content/Sort';
 import Title from './containers/Content/Title';
 import Item from './containers/Content/Item';
+import { useEffect, useState } from 'react';
 
 function App() {
+    const [loader, setLoader] = useState(true);
+    const [pizzaList, setPizzaList] = useState([]);
+    // fetch запрос
+    useEffect(() => {
+        fetch('https://6356c4029243cf412f8d1d66.mockapi.io/items')
+            .then((res) => res.json())
+            .then((json) => {
+                setPizzaList(json);
+                setLoader(false);
+            });
+    }, []);
+
     return (
         <div>
             <div className='wrapper'>
@@ -18,11 +30,13 @@ function App() {
                             <Sort />
                         </div>
                         <Title />
-                        <div className='content__items'>
-                            {pizzaList.map((el, i) => (
-                                <Item key={i} {...el} />
-                            ))}
-                        </div>
+                        {(loader && 'Идет загрузка...') || (
+                            <div className='content__items'>
+                                {pizzaList.map((el, i) => (
+                                    <Item key={i} {...el} />
+                                ))}
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
